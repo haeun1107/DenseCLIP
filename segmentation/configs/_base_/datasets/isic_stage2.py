dedba7939cad41696a61bee302be6d5fd58753dc
haeun1107 <haeun9394@gmail.com>
2025-10-30 16:10:28 +0000

    Add ACDC, ISIC Stage2 code

---
+++ segmentation/configs/_base_/datasets/isic_stage2.py
@@ -0,0 +1,84 @@
+dataset_type = 'ISICDataset'
+data_root='data/ISIC'
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53],
+    std=[58.395, 57.12, 57.375],
+    to_rgb=True)
+
+crop_size = (512, 512)
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadISICAnnotations'),
+    dict(type='Resize', img_scale=(512, 512), ratio_range=(0.5, 2.0)),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(512, 512),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=4,
+    train=[
+        dict(  # 10% GT
+            type=dataset_type,
+            data_root=data_root,
+            img_dir='ISIC2018_Task1-2_Training_Input',
+            ann_dir='ISIC2018_Task1_Training_GroundTruth',
+            split='splits/isic_task1_train_10.txt',
+            img_suffix='.jpg',
+            seg_map_suffix='_segmentation.png',
+            pipeline=train_pipeline
+        ),
+        dict(  # 90% pseudo
+            type=dataset_type,
+            data_root=data_root,
+            img_dir='ISIC2018_Task1-2_Training_Input',
+            ann_dir='pseudo-label',
+            split='splits/isic_task1_train_90.txt',
+            img_suffix='.jpg',
+            seg_map_suffix='_segmentation.png',
+            pipeline=train_pipeline
+        ),
+    ],
+    
+    val=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='ISIC2018_Task1-2_Training_Input',
+        ann_dir='ISIC2018_Task1_Training_GroundTruth',
+        split='splits/isic_task1_val_fix.txt',
+        img_suffix='.jpg',
+        seg_map_suffix='_segmentation.png',
+        pipeline=test_pipeline),
+
+    test=dict(
+        type=dataset_type,
+        data_root=data_root,
+        img_dir='ISIC2018_Task1-2_Training_Input',
+        ann_dir='ISIC2018_Task1_Training_GroundTruth',
+        split='splits/isic_task1_test_fix.txt',
+        img_suffix='.jpg',
+        seg_map_suffix='_segmentation.png',
+        pipeline=test_pipeline),
+)
---
+++ segmentation/configs/denseclip_fpn_res50_512x512_80k_isic_stage2.py
@@ -0,0 +1,84 @@
+_base_ = [
+    '_base_/models/denseclip_r50.py',
+    '_base_/datasets/isic_stage2.py',
+    '_base_/default_runtime.py',
+    '_base_/schedules/schedule_80k.py'
+]
+
+custom_imports = dict(
+    imports=[
+        'mmseg.datasets.isic',
+        'mmseg.datasets.pipelines.load_isic_annotation'
+    ],
+    allow_failed_imports=False
+)
+
+NUM_CLASSES = 2
+
+model = dict(
+    type='DenseCLIP',
+    pretrained='pretrained/RN50.pt',
+    context_length=12,
+    text_head=False,
+
+    backbone=dict(
+        type='CLIPResNetWithAttention',
+        layers=[3, 4, 6, 3],
+        output_dim=1024,
+        input_resolution=512,
+        style='pytorch'),
+
+    text_encoder=dict(
+        type='CLIPTextContextEncoder',
+        context_length=16,
+        embed_dim=1024,
+        transformer_width=512,
+        transformer_heads=8,
+        transformer_layers=12,
+        style='pytorch'),
+
+    context_decoder=dict(
+        type='ContextDecoder',
+        transformer_width=256,
+        transformer_heads=4,
+        transformer_layers=3,
+        visual_dim=1024,
+        dropout=0.1,
+        outdim=1024,
+        style='pytorch'),
+
+    neck=dict(
+        type='FPN',
+        in_channels=[256, 512, 1024, 2048 + NUM_CLASSES],
+        out_channels=256,
+        num_outs=4),
+
+    decode_head=dict(
+        type='FPNHead',
+        num_classes=NUM_CLASSES,
+        loss_decode=dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+    ),
+
+    class_names=['background', 'lesion'],
+)
+
+lr_config = dict(policy='poly', power=0.9, min_lr=1e-6, by_epoch=False,
+                 warmup='linear', warmup_iters=1500, warmup_ratio=1e-6)
+
+optimizer = dict(type='AdamW', lr=1e-4, weight_decay=1e-4,
+    paramwise_cfg=dict(
+        custom_keys={
+            'backbone':         dict(lr_mult=1.0),
+            'text_encoder':     dict(lr_mult=0.0),
+            'context_decoder':  dict(lr_mult=0.0),
+            'neck':             dict(lr_mult=0.0),
+            'decode_head':      dict(lr_mult=0.0),
+            'norm':             dict(decay_mult=0.)
+        }
+    )
+)
+
+data = dict(samples_per_gpu=4)
+evaluation = dict(metric=['mIoU', 'mDice'])
+load_from = 'work_dirs/ISIC/denseclip_fpn_res50_512x512_80k_isic_fix/iter_61600.pth'
+device = 'cuda'
